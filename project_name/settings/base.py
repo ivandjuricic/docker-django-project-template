1d2b9f1ffb329b3252aca2bebed5cfddeb0a90b4
Ivan Djuricic <ivan.djuricic@symphony.is>
2019-10-07 15:05:25 +0200

    host

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -82,9 +82,10 @@ WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_HOST"),
+        'NAME': os.getenv("DB_NAME"),
+        'HOST': os.getenv("DB_HOST"),
         'USER': os.getenv("DB_USER"),
-        'HOST': os.getenv("DB_PASSWORD"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'PORT': os.getenv("DB_PORT"),
     }
 }
